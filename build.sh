faf9036900d7cf7b99aeaca44b2381266d3d1722
MaksMurashko <maksmd251203@gmail.com>
2025-10-07 02:30:17 +0300

    first part - 2.2

--- build.sh
+++ build.sh
@@ -4,17 +4,14 @@ set -o errexit
 echo "Building frontend..."
 cd frontend
 npm install
-# --- ИСПРАВЛЕНИЕ: Передаем URL API в Vite ---
 VITE_API_URL=${RENDER_EXTERNAL_URL}/api npm run build
-# ---------------------------------------------
 cd ..
 
 echo "Installing backend dependencies..."
 pip install -r backend/requirements.txt
 
-export PYTHONPATH=$(pwd)
-
+# Устанавливаем PYTHONPATH перед каждой командой Django
 echo "Running Django management commands..."
-python backend/manage.py collectstatic --no-input --clear
-python backend/manage.py migrate
-python backend/create_superuser.py
+PYTHONPATH=$(pwd) python backend/manage.py collectstatic --no-input --clear
+PYTHONPATH=$(pwd) python backend/manage.py migrate
+PYTHONPATH=$(pwd) python backend/create_superuser.py
