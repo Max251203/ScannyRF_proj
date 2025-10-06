c267295533d4846f1759b7e0bb837b3ba48ec279
MaksMurashko <maksmd251203@gmail.com>
2025-10-06 23:36:31 +0300

    8.2

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ set -o errexit
 echo "Building frontend..."
 cd frontend
 npm install
-npm run build
+# --- ИСПРАВЛЕНИЕ: Передаем URL API в Vite ---
+VITE_API_URL=${RENDER_EXTERNAL_URL}/api npm run build
+# ---------------------------------------------
 cd ..
 
 echo "Installing backend dependencies..."
